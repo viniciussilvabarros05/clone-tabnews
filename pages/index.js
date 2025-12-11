f8fa3b3e13cfb6ca5abb661950e61e78ebc7d260
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2025-12-11 13:39:57 +0000

    Adicionar scripts de `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Hello World, amegaaaan. Não deu certo </h1>
+function Home() {
+  return <h1>Hello World, amegaaaan. Não deu certo </h1>;
 }
 
-export default Home
+export default Home;
