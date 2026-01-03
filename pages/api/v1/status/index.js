72ae30967a581dd31977ff87bd303c76e4170096
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2026-01-03 20:36:15 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Tão deixando a gente sonhar" });
+  const updatedAt = new Date().toISOString();
+  const datname = process.env.POSTGRES_DB;
+  const settings = await database.query(`SHOW server_version`);
+  const databaseMxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+
+  const databaseOpenedConnectionosResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [datname],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionosResult.rows[0].count;
+  const stats = { ...settings.rows[0], ...databaseMxConnectionsResult.rows[0] };
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: stats.server_version,
+        max_connections: parseInt(stats.max_connections),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
