1724e5475a23394735e3ca79fc4973d69ba5be22
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2026-08-22 02:12:20 +0000

    fix(fix handle client as undefined in database.js): fix handle cliente as undefined in database.js

--- infra/database.js
+++ infra/database.js
@@ -6,8 +6,11 @@ async function query(queryObject) {
     client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
+  }catch(error){
+    console.error(error)
+    throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 
