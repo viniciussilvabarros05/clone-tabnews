fc86db16a0691f3c55d75cd96322752c5428d093
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2026-08-29 19:39:00 +0000

    refactor: use `next-connect` in `/migrations` and `/status`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -24,3 +24,27 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
 });
+
+test("PUT or DELETE to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "PUT",
+  });
+  const responseBody = await response.json();
+  expect(responseBody).toEqual({
+    name: "MethodNotAllowedError",
+    message: "Método não permitido para este endpoint.",
+    action: "Verifique se o método HTTP enviado é valido para este endpoint.",
+    status_code: 405,
+  });
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  const response2Body = await response2.json();
+  expect(response2Body).toEqual({
+    name: "MethodNotAllowedError",
+    message: "Método não permitido para este endpoint.",
+    action: "Verifique se o método HTTP enviado é valido para este endpoint.",
+    status_code: 405,
+  });
+});
