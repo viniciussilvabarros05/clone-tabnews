172b40799af13eb4accffd2a99efafdd32b7e6ed
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2026-09-08 00:07:05 +0000

    feat: add user creation migration

---
+++ infra/migrations/1788819274081_create-users.js
@@ -0,0 +1,32 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    //For reference, github liits usernames to 39 characters.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+    },
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "varchar(72)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+    update_at: {
+      type: "timestamptz",
+      default: pgm.func("now()"),
+    },
+  });
+};
+exports.down = false;
